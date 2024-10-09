3b0bb41e848c7d7dd4e992700ef9d27890362092
Gautam Kumar <gautam.gk004@gmail.com>
2024-10-09 10:37:27 +0530

    Create 8-10-24.java

---
+++ 8-10-24.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minSwaps(String s) {
+        int size = 0;
+        int i = 0;
+        char top = 'x';
+        while(i < s.length()) {
+            char ch = s.charAt(i);
+            if(ch == '[') {
+                top = '[';
+                size++;
+            }
+
+            else if(ch == ']' && size > 0 && top == '[') {
+                size--;
+            }
+
+            else if(ch == ']' && size > 0 && top == ']') {
+                top = ']';
+                size++;
+            }
+            i++;
+        }
+        return (size+1)/2;
+    }
+}
+
+//Approach 1 (using stack)
+/*
+Used stack to remove the balanced Strings and then count the number of closing brackets left in the stack. 
+There is a fixed pattern (n+1)/2 is the min swaps.
+for ex. ] ] ] [ [ [ 
+this kind of pattern is always left in the stack when we remove the balanced string 
+so we divide it by 2, and +1 to handle odd number cases.
+*/ 
+
+
+//Approach 2 (Space Optimized)
+/*
+Used just 2 variables - size and top to remove the balanced Strings and then count the number of closing brackets in the unbalanced String
+There is a fixed pattern (n+1)/2 is the min swaps.
+for ex. ] ] ] [ [ [ 
+this kind of pattern is always left in the stack when we remove the balanced string 
+so we divide it by 2, and +1 to handle odd number cases.
+*/
