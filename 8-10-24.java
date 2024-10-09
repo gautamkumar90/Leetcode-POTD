3ca9d2813df0cffdf9c9bd22dbd66f5313f62b12
Gautam Kumar <gautam.gk004@gmail.com>
2024-10-09 10:38:37 +0530

    Update 8-10-24.java

--- 8-10-24.java
+++ 8-10-24.java
@@ -1,3 +1,47 @@
+//Approach 1 (using stack)
+/*
+Used stack to remove the balanced Strings and then count the number of closing brackets left in the stack. 
+There is a fixed pattern (n+1)/2 is the min swaps.
+for ex. ] ] ] [ [ [ 
+this kind of pattern is always left in the stack when we remove the balanced string 
+so we divide it by 2, and +1 to handle odd number cases.
+*/ 
+class Solution {
+    public int minSwaps(String s) {
+        Stack<Character> st = new Stack<>();
+        int i = 0;
+        while(i < s.length()) {
+            char ch = s.charAt(i);
+            if(ch == '[') {
+                st.push(ch);
+                // i++;
+            }
+
+            else if(ch == ']' && st.size() > 0 && st.peek() == '[') {
+                st.pop();
+                // i++;
+            }
+
+            else if(ch == ']' && st.size() > 0 && st.peek() == ']') {
+                st.push(ch);
+                // i++;
+            }
+            i++;
+        }
+        int len = st.size();
+        return (len+1)/2;
+    }
+}
+
+//Approach 2 (Space Optimized)
+/*
+Used just 2 variables - size and top to remove the balanced Strings and then count the number of closing brackets in the unbalanced String
+There is a fixed pattern (n+1)/2 is the min swaps.
+for ex. ] ] ] [ [ [ 
+this kind of pattern is always left in the stack when we remove the balanced string 
+so we divide it by 2, and +1 to handle odd number cases.
+*/
+
 class Solution {
     public int minSwaps(String s) {
         int size = 0;
@@ -23,22 +67,3 @@ class Solution {
         return (size+1)/2;
     }
 }
-
-//Approach 1 (using stack)
-/*
-Used stack to remove the balanced Strings and then count the number of closing brackets left in the stack. 
-There is a fixed pattern (n+1)/2 is the min swaps.
-for ex. ] ] ] [ [ [ 
-this kind of pattern is always left in the stack when we remove the balanced string 
-so we divide it by 2, and +1 to handle odd number cases.
-*/ 
-
-
-//Approach 2 (Space Optimized)
-/*
-Used just 2 variables - size and top to remove the balanced Strings and then count the number of closing brackets in the unbalanced String
-There is a fixed pattern (n+1)/2 is the min swaps.
-for ex. ] ] ] [ [ [ 
-this kind of pattern is always left in the stack when we remove the balanced string 
-so we divide it by 2, and +1 to handle odd number cases.
-*/
